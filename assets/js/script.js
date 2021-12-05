487323390ea6e89e2fea173433f73ab16f19acf6
Waffles <knharman@gmail.com>
2021-12-04 20:05:09 -0800

    color coded according to current hour of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var hoursArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
 var currentDate = function() {
     var today = moment()
     var todayFormatted = today.format("dddd, MMMM Do")
@@ -26,6 +28,7 @@ var taskEditorSetup = function(hour, persistingTask) {
         var currentTextAreaText = $(this).val()
         $("#" + hour + "-taskSpan").text(currentTextAreaText).show()
     })
+    .val(persistingTask)
     .hide()
     .appendTo(taskInfo)
 
@@ -39,14 +42,36 @@ var putTaskLocalStorage = function(hourIndex, text) {
     } else {
         existing = JSON.parse(existing)
     }
+
     // existing will either be array of empty strings or legit task data
     existing[hourIndex] = text
     console.log(text)
     localStorage.setItem("workdayScheduler", JSON.stringify(existing))
 }
 
+var colorCode = function() {
+    // responisble for color coding the cells
+    // what is current hour of day
+    // step through each hour, set correct color by comparing current hour and cell count
+
+    var currentHourString = moment().format("ha") // 10am
+    // var currentHourString = "12pm" <-- use this for testing
+    var currentHourMoment = moment(currentHourString, "ha")
+
+    for(i=0; i<hoursArr.length; i++) {
+        $(hoursArr[i] + "-taskInfo").addClass("past")
+        var hoursArrMoment = moment(hoursArr[i], "ha")
+        if(hoursArrMoment.isBefore(currentHourMoment)) {
+            $("#" + hoursArr[i] + "-taskInfo").addClass("past")
+        } else if(currentHourString == hoursArr[i]) {
+            $("#" + hoursArr[i] + "-taskInfo").addClass("present")
+        } else {
+            $("#" + hoursArr[i] + "-taskInfo").addClass("future")
+        }
+    }
+}
+
 var calendarSetup = function() {
-    var hoursArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
     var persistingTasks =  localStorage.getItem("workdayScheduler")
     persistingTasks = JSON.parse(persistingTasks)
 
@@ -60,14 +85,14 @@ var calendarSetup = function() {
             id: hour + "-timeblock"
         })
 
-        let displayTime = $("<div>", {
+        $("<div>", {
             class: "col hour",
             id: hour + "-displayTime"
         }).text(hour).appendTo(timeblock)
         
         taskEditorSetup(hour, persistingTasks[i]).appendTo(timeblock)
 
-        let saveTask = $("<div>", {
+        $("<div>", {
             class: "col saveBtn",
             id: hour + "-saveTask"
         }).click(function() {
@@ -77,8 +102,9 @@ var calendarSetup = function() {
 
         timeblock.appendTo($("#timeblocks"))
     }
-    
+    setInterval(colorCode, 1000*60*10)
 }
 
 currentDate();
 calendarSetup();
+colorCode();
